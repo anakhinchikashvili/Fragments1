8abfcf0c4151d0d8708badb190d61934d3d7cb24
anakhinchikashvili <ana.khinchikashvili.1@btu.edu.ge>
2022-11-30 14:26:47 +0400

    Initial commit

---
+++ app/src/main/java/com/example/fragments1/fragments/DashboardFragment.kt
@@ -0,0 +1,34 @@
+package com.example.fragments1.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.fragment.findNavController
+import com.example.fragments1.R
+
+
+class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
+
+    private lateinit var editText: EditText
+    private lateinit var button: Button
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        editText = view.findViewById(R.id.editTextTextPersonName)
+        button = view.findViewById(R.id.button)
+
+        button.setOnClickListener {
+            val name = editText.text.toString()
+            val action = DashboardFragmentDirections.actionDashboardFragmentToNotificationFragment(name)
+            findNavController().navigate(action)
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/fragments1/fragments/NotificationFragment.kt
@@ -0,0 +1,21 @@
+package com.example.fragments1.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.fragments1.R
+
+class NotificationFragment : Fragment(R.layout.fragment_notification) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.textView).text = NotificationFragmentArgs.fromBundle(requireArguments()).name
+
+    }
+
+}
